d34b8197c0ee13437fb64e17d3c007af8a5b77b8
aureuneun <aureuneun@gmail.com>
2021-03-25 12:34:41 +0900

    refactor: read_only_fields

--- rooms/views.py
+++ rooms/views.py
@@ -8,16 +8,16 @@ from . import serializers
 class RoomsView(APIView):
     def get(self, request):
         rooms = models.Room.objects.all()[:5]
-        serializer = serializers.ReadRoomSerializer(rooms, many=True).data
+        serializer = serializers.RoomSerializer(rooms, many=True).data
         return Response(serializer)
 
     def post(self, request):
         if not request.user.is_authenticated:
             return Response(status=status.HTTP_401_UNAUTHORIZED)
-        serializer = serializers.WriteRoomSerializer(data=request.data)
+        serializer = serializers.RoomSerializer(data=request.data)
         if serializer.is_valid():
             room = serializer.save(user=request.user)
-            room_serializer = serializers.ReadRoomSerializer(room).data
+            room_serializer = serializers.RoomSerializer(room).data
             return Response(room_serializer, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
@@ -33,7 +33,7 @@ class RoomView(APIView):
     def get(self, request, pk):
         room = self.get_room(pk)
         if room is not None:
-            serializer = serializers.ReadRoomSerializer(room).data
+            serializer = serializers.RoomSerializer(room).data
             return Response(serializer)
         return Response(status=status.HTTP_404_NOT_FOUND)
 
@@ -42,12 +42,12 @@ class RoomView(APIView):
         if room is not None:
             if room.user != request.user:
                 return Response(status=status.HTTP_403_FORBIDDEN)
-            serializer = serializers.WriteRoomSerializer(
+            serializer = serializers.RoomSerializer(
                 room, data=request.data, partial=True
             )
             if serializer.is_valid():
                 room = serializer.save()
-                room_serializer = serializers.ReadRoomSerializer(room).data
+                room_serializer = serializers.RoomSerializer(room).data
                 return Response(room_serializer)
             return Response(status=status.HTTP_200_OK)
         return Response(status=status.HTTP_404_NOT_FOUND)
