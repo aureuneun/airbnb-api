d748747afa6a7a9710d892749416bdbd6767b228
aureuneun <aureuneun@gmail.com>
2021-03-26 12:55:57 +0900

    feat: search and pagination

--- rooms/urls.py
+++ rooms/urls.py
@@ -5,5 +5,6 @@ app_name = "rooms"
 
 urlpatterns = [
     path("", views.RoomsView.as_view(), name="rooms"),
+    path("search/", views.search_room_view, name="search-room"),
     path("<int:pk>/", views.RoomView.as_view(), name="detail"),
 ]
